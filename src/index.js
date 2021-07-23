b7b8c4a66496b6ccf889b3f82fc89a7fefe620d3
Balázs Bősze <b.bosze@gmail.com>
2021-07-24 00:53:21 +0200

    setup hot loader

--- src/index.js
+++ src/index.js
@@ -15,6 +15,21 @@ ReactDOM.render(
   document.getElementById('root'),
 );
 
+if (module.hot) {
+  module.hot.accept('./components/App/App', () => {
+    // eslint-disable-next-line global-require
+    const NextApp = require('./components/App/App').default;
+    ReactDOM.render(
+      <React.StrictMode>
+        <ThemeProvider theme={theme}>
+          <NextApp />
+        </ThemeProvider>
+      </React.StrictMode>,
+      document.getElementById('root'),
+    );
+  });
+}
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
